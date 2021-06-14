6cbbd8a73b6c8466e8717ba90a05f0fad4465d50
Moritz Sokoll <moritz@sokoll.com>
2021-06-14 08:11:22 +0200

    added testing stage and first implementation of running brainfuck code (no loops and no IO)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 mod brainfuck;
+use brainfuck::interpreter::*;
 fn main() {
-    println!("Hello, world!");
+	let init_ptr: usize = 0;
+	let init_mem: Vec<u8> = vec![0; 16];
+    let state: ProgramState = ProgramState::new(init_mem, init_ptr);
+	let new_state = execute_statements(&vec!['+', '>', '-', '>', '+'], &state);
+	dbg!(new_state);
 }
